457a811dcb7205dbac58a9fec3021f42f5a5b584
m0rkovka <104685070+m0rkovka@users.noreply.github.com>
2022-06-04 17:25:10 +0300

    lugkbhglnvlvjgnvkglvncdshyjkyflgkuykdsreshtfgh

--- game.py
+++ game.py
@@ -22,6 +22,14 @@ class LeftPlayer(GamePlayer):
         elif keys[K_s] and self.rect.y < 500 - self.height:
             self.rect.y += self.step
 
+class RightPlayer(GamePlayer):
+    def update(self):
+        keys = key.get_pressed()
+        if keys[K_i] and self.rect.y > 0:
+            self.rect.y -= self.step
+        elif keys[K_k] and self.rect.y < 500 - self.height:
+            self.rect.y += self.step
+
 
 window = display.set_mode((700, 500))
 background = transform.scale(image.load('stol.png'), (700, 500))
@@ -29,12 +37,29 @@ fps = 60
 clock = time.Clock()
 game = True
 player_1 = LeftPlayer('raketka.jpg', 40, 80, 40, 210, 10)
+player_2 = RightPlayer('raketka.jpg', 40, 80, 620, 210, 10)
+player_2.image = transform.rotate(player_2.image, 180)
+ball = GamePlayer('mach.png', 20, 20, 340, 240, 10)
+dx = 3
+dy = 3
+
+
 while game:
     for e in event.get():
         if e.type == QUIT:
             game = False
-window.blit(background, (0, 0))
-player_1.update()
-player_1.reset()
-display.update()
-clock.tick(fps)
+    if ball.rect.x >= 680 or ball.rect.x <= 0:
+        dx *= -1
+    if ball.rect.y >= 480 or ball.rect.y <= 0:
+        dy *= -1
+    ball.rect.x += dx
+    ball.rect.y += dy
+    window.blit(background, (0, 0))
+    player_1.update()
+    player_1.reset()
+    player_2.update()
+    player_2.reset()
+    ball.update()
+    ball.reset()
+    display.update()
+    clock.tick(fps)
